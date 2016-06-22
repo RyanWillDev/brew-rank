65590c9b16985781d90f61b0c89f839a5c20691d
Ryan  Will <ryanwilldev@gmail.com>
2016-06-22 13:14:47 -0500

    changed port to either be the Heroku env port or 3000

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Set the port to the Heroku ENV variable or 3000
+let port = process.env.PORT || 3000;
+
 // Require Express
 const express = require('express');
 
@@ -7,10 +10,12 @@ const app = express();
 // Require Mongoose
 const mongoose = require('mongoose');
 
+// Connect mongoose to database
 mongoose.connect('mongodb://localhost/brew-rank');
 
+// Alias the connection as db
 const db = mongoose.connection;
 
 app.get('/', (request, response) => response.send('Hello World'));
 
-app.listen(3000, () => console.log('App is running'));
+app.listen(port, () => console.log('App is running'));
