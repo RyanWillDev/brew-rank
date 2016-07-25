53ca60d7f9eff6be9f0bc1454a14ea1eafb52f77
Ryan  Will <ryanwilldev@gmail.com>
2016-07-25 11:06:03 -0500

    added handleSubmit for form, set up checks for >=21, matching passwords, passwords >= 8 characters

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,27 +1,91 @@
 import React, { Component } from 'react';
 
 export default class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      alertClass: '',
+      message: '',
+    };
+  }
+
+  is21() {
+    // Get today's date
+    const today = new Date();
+    // Get the date provided by user
+    const birthDate = new Date(this.refs.birthday.value);
+    // Get age of user
+    const age = today.getFullYear() - birthDate.getFullYear();
+    // Return true or false depending on the age
+    // this will be used to either alert the user they
+    // are too young or pass the last check before submitting form.
+    return age >= 21;
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // Verify the password has been entered exactly the same both times
+    if (this.refs.pw.value !== this.refs.pwCheck.value) {
+      this.setState({
+        message: 'Passwords do not match.',
+        alertClass: 'alert alert-danger',
+      });
+      // Verify the password is at least 8 characters
+    } else if (!(this.refs.pw.value.length >= 8)) {
+      this.setState({
+        message: 'Passwords must be at least 8 characters.',
+        alertClass: 'alert alert-danger',
+      });
+      // Make sure the user is at least 21
+    } else if (!this.is21()) {
+      this.setState({
+        message: 'Oooh... come back when your 21',
+        alertClass: 'alert alert-danger',
+      });
+    } else {
+      this.setState({
+        message: '',
+        alertClass: '',
+      });
+      console.log('submitting');
+    }
+  }
+
   render() {
     return (
       <div className="container">
         <h1 className="text-center">Glad you're joining us!</h1>
-        <form action="localhost:3000/restapi/users" method="post">
+        <form action="localhost:3000/restapi/users" method="post" onSubmit={this.handleSubmit}>
           <div className="row">
             <div className="form-group">
               <div className="col-sm-3 col-sm-offset-3">
-                <input type="text" className="form-control" placeholder="First Name" required />
+                <input type="text" className="form-control"
+                  placeholder="First Name" ref="firstName" required
+                />
               </div>
                <div className="col-sm-3">
-                <input type="text" className="form-control" placeholder="Last Name" required />
+                <input type="text" className="form-control"
+                  placeholder="Last Name" ref="lastName" required
+                />
               </div>
               <div className="col-sm-6 col-sm-offset-3">
-                <input type="email" className="form-control" placeholder="Email Address" required />
+                <input type="email" className="form-control"
+                  placeholder="Email Address" ref="email" required
+                />
               </div>
               <div className="col-sm-6 col-sm-offset-3">
-                <input type="password" className="form-control" placeholder="Password" required />
+                <input type="password" className="form-control"
+                  placeholder="Password" ref="pw" required
+                />
               </div>
               <div className="col-sm-6 col-sm-offset-3">
-                <input type="date" required />
+                <input type="password" className="form-control"
+                  placeholder="Re-enter Password" ref="pwCheck" required
+                />
+              </div>
+              <div className="col-sm-6 col-sm-offset-3">
+                <input type="date" ref="birthday" required />
               </div>
               <div className="col-sm-12 col-sm-offset-3">
                 <button type="submit" className="btn btn-default">Submit</button>
@@ -29,6 +93,9 @@ export default class SignUpForm extends Component {
             </div>
           </div>
         </form>
+        <div className="row">
+          <div className={this.state.alertClass}>{this.state.message}</div>
+        </div>
       </div>
     );
   }
