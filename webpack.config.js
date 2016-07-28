5ff56e3f41c72d71cf397bec65970051eb4d7ee9
Ryan  Will <ryanwilldev@gmail.com>
2016-07-28 13:27:56 -0500

    added plugin for babel to handle object spread, reconfigured babel with .babelrc, changed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel?presets[]=es2015,presets[]=react'],
+        loaders: ['react-hot', '[babel]'],
       },
       {
         test: /\.html$/,
