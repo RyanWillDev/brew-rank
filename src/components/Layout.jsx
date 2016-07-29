f9bc58fd06d48a2d9c443d418189bed6cb7fe912
Ryan  Will <ryanwilldev@gmail.com>
2016-07-29 16:11:22 -0500

    forgot to export Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 // Import components
 import Nav from './Nav';
 
-const Layout = () => (
+const Layout = (props) => (
   <div className="layout-container">
     <Nav />
-    {React.cloneElement(this.props.children, this.props)}
+    {React.cloneElement(props.children, props)}
   </div>
 );
 
 Layout.propTypes = {
   children: React.PropTypes.object,
 };
+
+export default Layout;
