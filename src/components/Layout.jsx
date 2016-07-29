893b10e6802d8e44229dd14f1ef20e4fd939e579
Ryan  Will <ryanwilldev@gmail.com>
2016-07-29 15:33:34 -0500

    refactored Layout as stateless component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,14 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 
 // Import components
 import Nav from './Nav';
 
-export default class Layout extends Component {
-  render() {
-    return (
-      <div className="layout-container">
-        <Nav />
-        {React.cloneElement(this.props.children, this.props)}
-      </div>
-    );
-  }
-}
+const Layout = () => (
+  <div className="layout-container">
+    <Nav />
+    {React.cloneElement(this.props.children, this.props)}
+  </div>
+);
 
 Layout.propTypes = {
   children: React.PropTypes.object,
