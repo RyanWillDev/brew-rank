bd786df6580e14fd36407389e6a6558ecfc4f227
Ryan  Will <ryanwilldev@gmail.com>
2016-08-06 11:25:22 -0500

    tracks config for heroku

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  secret: '332dfsiidkpopin$$5',
+  development: 'mongodb://localhost/brew-rank',
+  production: 'mongodb://heroku:heroku@ds145245.mlab.com:45245/heroku_79f5th7r',
+};
